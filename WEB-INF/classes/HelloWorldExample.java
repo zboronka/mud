1473e31de9057c8686531a611c448230d4fb22cc
Zachary Kurt Boronka <zboronka@gmail.com>
2020-02-19 11:47:00 -0500

    Initial commit

---
+++ WEB-INF/classes/HelloWorldExample.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldExample extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+		out.println("Hello World!");
+    }
+}
+
+
+
